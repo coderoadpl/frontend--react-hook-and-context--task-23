30f6c747df4b106fcb5239503f7b1f1a969e8e4e
Mateusz Choma <kontakt@coderoad.pl>
2021-10-16 11:53:10 +0200

    Basic router implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from 'react'
 
+import Button from './components/Button'
+
 import PageOne from './pages/PageOne'
 import PageTwo from './pages/PageTwo'
 
 export const App = () => {
+  const [currentPage, setCurrentPage] = React.useState('page-one')
   return (
     <div>
-      <PageOne />
-      <PageTwo />
+      <Button
+        disabled={currentPage === 'page-one'}
+        onClick={() => setCurrentPage(() => 'page-one')}
+      >
+        Go to PageOne
+      </Button>
+      <Button
+        disabled={currentPage === 'page-two'}
+        onClick={() => setCurrentPage(() => 'page-two')}
+      >
+        Go to PageTwo
+      </Button>
+      {
+        currentPage === 'page-one' ?
+          <PageOne />
+          :
+          null
+      }
+      {
+        currentPage === 'page-two' ?
+          <PageTwo />
+          :
+          null
+      }
     </div>
   )
 }

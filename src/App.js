50e868b30747df8a0f3e8f50ea102cf852eb821c
Mateusz Choma <kontakt@coderoad.pl>
2021-10-16 12:46:58 +0200

    Using `ThemeContext.Consumer` in `App` component

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import PageTwo from './pages/PageTwo'
 import { ThemeContext } from './ThemeContext'
 import { createTheme } from './theme'
 
-const theme = createTheme('green')
-
 export const App = () => {
   const [currentPage, setCurrentPage] = React.useState('page-one')
   const [mainColor, setMainColor] = React.useState('green')
@@ -19,35 +17,45 @@ export const App = () => {
       value={createTheme(mainColor)}
     >
       <div>
-        <Button
-          style={{ backgroundColor: theme.mainColor }}
-          disabled={currentPage === 'page-one'}
-          onClick={() => setCurrentPage(() => 'page-one')}
-        >
-          Go to PageOne
-        </Button>
-        <Button
-          style={{ backgroundColor: theme.mainColor }}
-          disabled={currentPage === 'page-two'}
-          onClick={() => setCurrentPage(() => 'page-two')}
-        >
-          Go to PageTwo
-        </Button>
-        <br />
-        <Button
-          style={{ backgroundColor: theme.mainColor }}
-          disabled={mainColor === 'green'}
-          onClick={() => setMainColor(() => 'green')}
-        >
-          Color green
-        </Button>
-        <Button
-          style={{ backgroundColor: theme.mainColor }}
-          disabled={mainColor === 'yellow'}
-          onClick={() => setMainColor(() => 'yellow')}
-        >
-          Color yellow
-        </Button>
+        <ThemeContext.Consumer>
+          {
+            (theme) => {
+              return (
+                <>
+                  <Button
+                    style={{ backgroundColor: theme.mainColor }}
+                    disabled={currentPage === 'page-one'}
+                    onClick={() => setCurrentPage(() => 'page-one')}
+                  >
+                    Go to PageOne
+                  </Button>
+                  <Button
+                    style={{ backgroundColor: theme.mainColor }}
+                    disabled={currentPage === 'page-two'}
+                    onClick={() => setCurrentPage(() => 'page-two')}
+                  >
+                    Go to PageTwo
+                  </Button>
+                  <br />
+                  <Button
+                    style={{ backgroundColor: theme.mainColor }}
+                    disabled={mainColor === 'green'}
+                    onClick={() => setMainColor(() => 'green')}
+                  >
+                    Color green
+                  </Button>
+                  <Button
+                    style={{ backgroundColor: theme.mainColor }}
+                    disabled={mainColor === 'yellow'}
+                    onClick={() => setMainColor(() => 'yellow')}
+                  >
+                    Color yellow
+                  </Button>
+                </>
+              )
+            }
+          }
+        </ThemeContext.Consumer>
         {
           currentPage === 'page-one' ?
             <PageOne />
